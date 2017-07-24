ff88928b4a44e2111505707153e8ce87c96ac54a
Omar Rampado <omar.rampado@larus-ba.it>
2017-07-24 17:39:48 +0200

    Add copyright

--- src/main/java/it/larus/jqassistant/plugin/nmap/NMapFileScannerPlugin.java
+++ src/main/java/it/larus/jqassistant/plugin/nmap/NMapFileScannerPlugin.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2017 Larus Business Automation
+ *
+ * This file is part of "JQAssistant Nmap plugin".
+ *
+ * "JQAssistant Nmap plugin" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * "JQAssistant Nmap plugin" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.larus.jqassistant.plugin.nmap;
 
 import com.buschmais.jqassistant.core.scanner.api.Scanner;
